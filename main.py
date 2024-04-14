8e909a81753491c68e674d362b2520458208486b
HarshHRK07 <149139740+HarshHRK07@users.noreply.github.com>
2024-04-14 17:09:22 +0530

    Update and rename main1.py to main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from keep_alive import keep_alive
 keep_alive()
 
 # Telegram Bot Token
-TOKEN = '7195510626:AAGjTRYddWdXlehlJCG1W06rb3vzlQzbnzk'
+TOKEN = '7195510626:AAElLeSHqTB4WR4N3vY0qW-cXH_zplF3G7I'
 bot = telebot.TeleBot(TOKEN)
 
 # OpenAI API Key
